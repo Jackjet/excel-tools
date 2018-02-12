b82f2711ce84a9af543b441e8d69b03eed0ff55c
Liqiaolin <992004863@qq.com>
2018-02-12 15:35:22 +0800

    新增导出内容 靠齐枚举

---
+++ src/main/java/indi/qiaolin/excel/tools/enumerate/Alignment.java
@@ -0,0 +1,29 @@
+package indi.qiaolin.excel.tools.enumerate;
+
+/**
+ *  导出对齐方法
+ * @author qiaolin
+ * @dete 2018年2月11日
+ * 
+ */
+
+public enum Alignment {
+	/**
+	 * 自动
+	 */
+	auto,
+	/**
+	 *  向左对齐
+	 */
+	left,
+	
+	/**
+	 *  居中对齐
+	 */
+	center,
+	
+	/**
+	 *  向右对齐
+	 */
+	right
+}
