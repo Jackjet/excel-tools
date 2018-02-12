89b50384e31a24604cfbd20aef0669550c0e0f59
Liqiaolin <992004863@qq.com>
2018-02-12 15:35:55 +0800

    新增 Excel 部位 样式类型枚举

---
+++ src/main/java/indi/qiaolin/excel/tools/enumerate/style/StyleType.java
@@ -0,0 +1,23 @@
+package indi.qiaolin.excel.tools.enumerate.style;
+
+/**
+ *  单元格样式类型
+ * @author qiaolin
+ *
+ */
+
+public enum StyleType {
+	/**
+	 *  标题样式
+	 */
+	title,
+	/**
+	 * 表头样式
+	 */
+	header,
+	/**
+	 * 内容样式
+	 */
+	body
+	
+}
